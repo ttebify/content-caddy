197029aace52a79b7d32db08b1f94d1ff3a9f0b5
Priyanko Das <pridas2@WKMZT0210FEC.global.publicisgroupe.net>
2023-04-08 15:16:08 +0530

    remove developer details

--- src/pages/popup/sections/ContactDeveloper.tsx
+++ src/pages/popup/sections/ContactDeveloper.tsx
@@ -5,24 +5,10 @@ function ContactDeveloper() {
     <div className="container">
       <div className="contact">
         <div>
-          <a href="https://github.com/ttebify/content-caddy" target="_blank">https://github.com/ttebify/content-caddy</a>
+          <a href="https://github.com/ttebify/content-caddy" target="_blank"><h2>https://github.com/ttebify/content-caddy</h2></a>
         </div>
-        <h2>Justice Ekemezie</h2>
-        <p>I illuminate concepts with code and prose</p>
-        <div className="icons">
-          <a href="#" className="icon">
-            <img src="/assets/icons/github.png" alt="GitHub" />
-          </a>
-          <a href="#" className="icon">
-            <img src="/assets/icons/twitter.png" alt="Twitter" />
-          </a>
-          <a href="#" className="icon">
-            <img src="/assets/icons/linkedin.png" alt="Linkedin" />
-          </a>
-          <a href="#" className="icon">
-            <img src="/assets/icons/mail.png" alt="Email" />
-          </a>
-        </div>
+       
+        
       </div>
     </div>
   );
